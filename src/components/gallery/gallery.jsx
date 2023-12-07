ea8803a46795b32fea7fdabc8e5ac667f6b5f4eb
Kyle Abrahamse <kyleabrahamse@outlook.com>
2023-12-07 19:17:09 +0000

    Added photos from local storage

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -6,9 +6,9 @@ function Gallery() {
             <h1>Gallery</h1>
             <div className="img-container">
                 <img className="img-1" src="images/tree.jpg" alt="" />
-                <img className="img-2" src="https://scontent.flba3-1.fna.fbcdn.net/v/t39.30808-6/362292092_10219439530378402_3684268133705059333_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=c42490&_nc_ohc=J0MfRcgu0xQAX_u6XBg&_nc_ht=scontent.flba3-1.fna&oh=00_AfBf5TWjy2p3iqaE2ggT8xK_sDHXs2Pj1cxB66zNsfzXWw&oe=656F93CA" alt="" />
-                <img className="img-3" src="images/baby-leopard.jpg" alt="baby leopard" />
-                <img className="img-4" src="https://scontent.flba3-2.fna.fbcdn.net/v/t39.30808-6/402319966_10219888776769281_7375356042403682196_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=c42490&_nc_ohc=9YB2X_wuzNsAX_ZFkj3&_nc_ht=scontent.flba3-2.fna&oh=00_AfAbDRihT6ZnBS4X79mdWqMtkOXQGSnnyxCOusmR4kHn1g&oe=65704910" alt="" />
+                <img className="img-2" src="images/baby-leopard.jpg" alt="baby leopard" />
+                <img className="img-3" src="images/light.jpg" alt="leopard in sun" />
+                <img className="img-4" src="images/treehouse.jpg" alt="" />
             </div>
             <h2>View more</h2>
         </div>
