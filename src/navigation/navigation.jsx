8fb057afe41ab7d59199fbe216ab3b1adec8fb21
Kyle Abrahamse <kyleabrahamse@outlook.com>
2024-02-15 21:12:57 +0000

    all pages are now responsive

--- src/navigation/navigation.jsx
+++ src/navigation/navigation.jsx
@@ -12,47 +12,19 @@ function Nav() {
     setIsOpen((open) => !open);
   }
 
+  // function closeMenu() {
+  //   setIsOpen(false);
+  // }
+
   return (
     <div>
       {/* <Headroom> */}
-        <div className="nav-bar">
-          <div className="nav-left">
-            <Link to="/">
-              <img src="images/logo-1.png" alt="logo" />
-            </Link>
-            <ul className="menu-close">
-              <Link className="link" to="/tours">
-                <li>Tours</li>
-              </Link>
-              <Link className="link" to="/gallery">
-                <li>Gallery</li>
-              </Link>
-              <Link className="link" to="/leopard-profiles">
-                <li>Leopard profiles</li>
-              </Link>
-              <Link className="link" to="/sightings">
-                <li>Sightings</li>
-              </Link>
-              <Link className="link" to="conservation">
-                <li>Conservation</li>
-              </Link>
-            </ul>
-          </div>
-          <div className="nav-btn-container">
-            <Button buttonType="white" text="Contact" />
-            <Button text="Support" />
-          </div>
-          <i
-            className={
-              isOpen
-                ? "fa-solid fa-x burger-menu"
-                : "fa-solid fa-bars burger-menu"
-            }
-            onClick={burgers}
-          ></i>
-        </div>
-        <div className={`menu-container ${isOpen ? 'menu-open' : 'menu-close'}`}>
-          <ul className="content">
+      <div className="nav-bar">
+        <div className="nav-left">
+          <Link to="/">
+            <img src="images/logo-1.png" alt="logo" />
+          </Link>
+          <ul className="menu-close">
             <Link className="link" to="/tours">
               <li>Tours</li>
             </Link>
@@ -70,6 +42,38 @@ function Nav() {
             </Link>
           </ul>
         </div>
+        <div className="nav-btn-container">
+          <Button buttonType="white" text="Contact" />
+          <Button text="Support" />
+        </div>
+        <i
+          className={
+            isOpen
+              ? "fa-solid fa-x burger-menu"
+              : "fa-solid fa-bars burger-menu"
+          }
+          onClick={burgers}
+        ></i>
+      </div>
+      <div className={`menu-container ${isOpen ? "menu-open" : "menu-close"}`}>
+        <ul className="content">
+          <Link className="link" to="/tours" onClick={burgers}>
+            <li>Tours</li>
+          </Link>
+          <Link className="link" to="/gallery" onClick={burgers}>
+            <li>Gallery</li>
+          </Link>
+          <Link className="link" to="/leopard-profiles" onClick={burgers}>
+            <li>Leopard profiles</li>
+          </Link>
+          <Link className="link" to="/sightings" onClick={burgers}>
+            <li>Sightings</li>
+          </Link>
+          <Link className="link" to="conservation" onClick={burgers}>
+            <li>Conservation</li>
+          </Link>
+        </ul>
+      </div>
       {/* </Headroom> */}
       <Outlet />
     </div>
